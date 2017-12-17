b95beaa12121a1015c15a0d80564063d14b74361
Piotr Furmanek <pete85@users.noreply.github.com>
2017-12-17 21:35:59 +0000

    new components added

--- loan-calculator/src/app/app.module.ts
+++ loan-calculator/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
@@ -12,10 +13,20 @@ import {
   MatInputModule,
   MatSelectModule,
 } from '@angular/material';
+import { SourcesComponent } from './sources/sources.component';
+import { PaymentsComponent } from './payments/payments.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SourcesComponent,
+    PaymentsComponent,
+    NavbarComponent,
+    HomeComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,7 +37,22 @@ import {
     FormsModule,
     MatListModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'sources',
+        component: SourcesComponent
+      },
+      {
+        path: '**',
+        component: NotFoundComponent
+      },
+
+    ])
   ],
   exports: [
     MatInputModule,
