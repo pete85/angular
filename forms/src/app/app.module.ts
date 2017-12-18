e054593288f3e5f146f39917b0b02457f441147d
Piotr Furmanek <pete85@users.noreply.github.com>
2017-12-18 16:35:45 +0000

    Bootstrap and new components

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,18 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// BOOTSTRAP
+import { AlertModule } from 'ngx-bootstrap';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+// ROUTER
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SourcesComponent } from './sources/sources.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavigationComponent,
+    NotFoundComponent,
+    SourcesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    AlertModule.forRoot(),
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'sources',
+        component: SourcesComponent
+      },
+      {
+        path: '**',
+        component: NotFoundComponent
+      },
+
+    ])
+  ],
+  exports: [
+    BsDropdownModule,
+    TooltipModule,
+    ModalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
