c76e049e6451f5e69eb61a58af2194425bdf1fc0
Piotr Furmanek <pete85@users.noreply.github.com>
2017-12-19 10:20:13 +0000

    new components

---
+++ forms/src/app/forms/forms.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginFormComponent } from './login-form/login-form.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+  ],
+  declarations: [LoginFormComponent]
+})
+export class ProjectFormsModule { }
