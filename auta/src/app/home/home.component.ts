7ddffcd45c3caea14e95e441054d50488bda26b5
Piotr Furmanek <pete85@users.noreply.github.com>
2018-04-03 16:10:49 +0100

    components setup

--- auta/src/app/home/home.component.ts
+++ auta/src/app/home/home.component.ts
@@ -10,6 +10,24 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    const mysql = require('mysql');
+    const connection = mysql.createConnection({
+      host     : 'pete85com1.ipagemysql.com',
+      user     : 'pete85',
+      password : 'Vectr@19CDti',
+      database : 'pete85portfolio'
+    });
+
+    connection.connect();
+
+    connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+      if (err) throw err
+      console.log('The solution is: ', rows[0].solution)
+    });
+
+    connection.end();
+
   }
 
 }
